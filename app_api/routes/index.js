7106834072256f505402c78590d77cdc8c3bd116
Samit Datta <samitdattaofficial@gmail.com>
2025-12-07 16:06:48 -0500

    Module 6 completed baseline

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const tripsController = require("../controllers/trips");
 
-router.route("/trips").get(tripsController.tripsList);
+router.route("/trips")
+    .get(tripsController.tripsList)
+    .post(tripsController.tripsAddTrip);
 
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode);
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
